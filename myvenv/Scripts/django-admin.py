d6088064b1ee7bd6f1d651b9f8d9bf1a3e204ab6
songsuhyeon <ssh1692@likelion.org>
2020-07-20 05:13:17 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ssh16\onedrive\바탕 화면\송수현\스터디\django 3주차 과제\homework-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
